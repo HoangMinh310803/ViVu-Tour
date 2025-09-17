532d0c619cd0d48355de2ea1155716f134c2a573
Minh <minhhoangdev3108@gmail.com>
2025-09-17 20:56:55 +0700

    tao cau truc code

---
+++ fe-vivu-tour/src/components/HeroSection.jsx
@@ -0,0 +1,14 @@
+// src/components/HeroSection.jsx
+import React from "react";
+
+function HeroSection() {
+  return (
+    <section className="hero">
+      <h1>Khám phá Việt Nam cùng Tour</h1>
+      <p>Tìm kiếm tour du lịch mơ ước của bạn</p>
+      {/* Thanh tìm kiếm */}
+    </section>
+  );
+}
+
+export default HeroSection;
---
+++ fe-vivu-tour/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+// import Header from "../components/Header";
+import HeroSection from "../components/HeroSection";
+// import TourHighlights from "../components/TourHighlights";
+// import Footer from "../components/Footer";
+
+function Home() {
+  return (
+    <div className="home-page">
+      <HeroSection />
+    </div>
+  );
+}
+
+export default Home;
