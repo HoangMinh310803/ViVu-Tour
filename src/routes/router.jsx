7552cc65993d676428d2b353600cb9da6165db6b
Minh <minhhoangdev3108@gmail.com>
2025-09-22 10:28:38 +0700

    update

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,10 +1,9 @@
 // src/routes/index.js (ví dụ)
 import { createBrowserRouter } from "react-router-dom";
-import Home from "../pages/HomePage";
+import HomePage from "../pages/HomePage";
 import AdminDashboard from "../components/admin/AdminDashboard";
 import LoginPage from "../pages/LoginPage";
 import RegisterPage from "../pages/RegisterPage";
-import TourDetail from "../pages/TourDetail";
 import TourSearchPage from "../components/TourSearchPage";
 
 import TourDetailPage from "../pages/TourDetailPage";
@@ -12,12 +11,6 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
-  },
-
-  {
-    path: "/tour/:id",
-    element: <TourDetail />,
-    element: <Home />,
   },
   {
     path: "/admin",
