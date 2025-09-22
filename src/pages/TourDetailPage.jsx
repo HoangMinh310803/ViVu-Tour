53053aa67d9b87eda04ebc4d84764d7c26f0681d
Kyeen <nhukien0603@gmail.com>
2025-09-22 11:38:33 +0700

    FIX DEPLOY 1

--- src/pages/TourDetailPage.jsx
+++ src/pages/TourDetailPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header1 from "../components/Header";
-import Header2 from '../components/TourDetail/Header/header';
+import Header2 from '../components/TourDetail/Header/Header';
 import MainContent from '../components/TourDetail/MainContent/MainContent';
 import FloatingButtons from '../components/TourDetail/FloatingButtons/FloatingButtons';
 import './TourDetailPage.css';
