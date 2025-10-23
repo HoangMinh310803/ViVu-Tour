c1dee71a6976caf9b21518038a26e0d346720b27
Minh Hoang <minhhoangdev3108@gmail.com>
2025-10-23 08:17:32 +0700

    update google analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 // src/App.jsx
-import React from "react";
+import React, { useEffect } from "react";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/router.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  useEffect(() => {
+    const unlisten = router.subscribe((state) => {
+      // Mỗi khi route thay đổi, gửi event cho GA
+      if (window.gtag) {
+        window.gtag("config", "G-RFCL5YKGVJ", {
+          page_path: state.location.pathname + state.location.search,
+        });
+      }
+    });
+
+    return () => unlisten(); // cleanup khi component unmount
+  }, []);
+
   return <RouterProvider router={router} />;
 }
 
